4f884611aba15c4a6839e7f509400e6cdafdfe9a
Yi Min Wang <yimin@Yis-MacBook-Pro.local>
2017-05-16 17:26:02 -0400

    dank memes

--- starting-point/browser/react/components/Album.js
+++ starting-point/browser/react/components/Album.js
@@ -1,26 +1,44 @@
 import React from 'react';
 import Songs from '../components/Songs';
 
-const Album = (props) => {
+class Album extends React.Component {
+  constructor (props){
+    super();
+    this.state = {
+       album : props.album,
+       currentSong : props.currentSong,
+       isPlaying : props.isPlaying,
+       toggleOne : props.toggleOne
+    }
+    // const album = props.album;
+    // const currentSong = props.currentSong;
+    // const isPlaying = props.isPlaying;
+    // const toggleOne = props.toggleOne;
+  }
 
-  const album = props.album;
-  const currentSong = props.currentSong;
-  const isPlaying = props.isPlaying;
-  const toggleOne = props.toggleOne;
+  componentDidMount () {
+    console.log(this.props.selectAlbum);
+    const albumId = this.props.routeParams.albumId;
+    const selectAlbum = this.props.selectAlbum;
 
-  return (
-    <div className="album">
-      <div>
-        <h3>{ album.name }</h3>
-        <img src={ album.imageUrl } className="img-thumbnail" />
+    selectAlbum(albumId);
+  }
+
+  render(){
+    return (
+      <div className="album">
+        <div>
+          <h3>{ this.state.album.name }</h3>
+          <img src={ this.state.album.imageUrl } className="img-thumbnail" />
+        </div>
+        <Songs
+          songs={this.state.album.songs}
+          currentSong={this.state.currentSong}
+          isPlaying={this.state.isPlaying}
+          toggleOne={this.state.toggleOne} />
       </div>
-      <Songs
-        songs={album.songs}
-        currentSong={currentSong}
-        isPlaying={isPlaying}
-        toggleOne={toggleOne} />
-    </div>
-  );
+    );
+  }
 }
 
 export default Album;
